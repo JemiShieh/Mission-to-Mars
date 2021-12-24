36193c5e2b6df523230b419ad0820f528817344c
JemiShieh <jemishieh@hotmail.com>
2021-12-23 23:09:40 -0500

    Updating scraping.py

--- scraping.py
+++ scraping.py
@@ -105,7 +105,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-dark table-bordered")
 
 # Create function
 def hemispheres(browser):
